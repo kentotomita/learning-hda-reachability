279c0220735d31976159b97683647f936fc26eb3
Kento <ktomita3@gatech.edu>
2023-11-22 11:17:02 -0500

    transform for 3-point reachset

--- src/learning/transform.py
+++ src/learning/transform.py
@@ -3,20 +3,20 @@ import torch
 
 # Parameters for NN input
 ALT_MAX = 1500.0
-ALT_MIN = 100.0
-TGO_MAX = 80.0
+ALT_MIN = 0.0
+TGO_MAX = 150.0
 TGO_MIN = 0.0
-VX_MAX = 50.0
+VX_MAX = 100.0
 VX_MIN = 0.0
-VZ_MAX = 20.0
-VZ_MIN = -90.0
-MASS_MAX = 1825.0
+VZ_MAX = 100.0
+VZ_MIN = -100.0
+MASS_MAX = 1905.0
 MASS_MIN = 1505.0
 Z_MAX = torch.log(torch.tensor(MASS_MAX))
 Z_MIN = torch.log(torch.tensor(MASS_MIN))
 
 # Parameters for NN output
-FOV = torch.tensor(15 * 3.1415926535 / 180)
+FOV = torch.tensor(0.2617993877991494)  # 15 degrees in radian
 FOV_R_MAX = ALT_MAX * torch.tan(FOV / 2)
 SMA_MAX = ALT_MAX * 10
 
@@ -51,30 +51,25 @@ def inverse_transform_ic(alt_, vx_, vz_, z_, tgo_):
     return alt, vx, vz, z, tgo
 
 
-def transform_reachsetparam(xmin, xmax, alpha, yp, a1, a2, alt, fov=FOV):
+def transform_reachsetparam(xmin, xmax, ymax, x_ymax, alt, fov=FOV):
     """transform values to [0, 1]"""
+    assert ymax >= 0, "ymax must be positive"
     # normalize
-    fov_radius = (alt * np.tan(fov / 2)) * 1.1  # 1.1 is a safety factor
-    length_max = fov_radius * 5
+    fov_radius = (alt * np.tan(fov / 2)) * 1.05  # 5% safety factor
     xmin_ = (xmin + fov_radius) / fov_radius / 2
-    xmax_ = (xmax - xmin) / fov_radius / 2
-    alpha_ = alpha
-    yp_ = yp / fov_radius
-    a1_ = (a1 - alpha * (xmax - xmin) / 2) / length_max
-    a2_ = (a2 - (1 - alpha) * (xmax - xmin) / 2) / length_max
+    xmax_ = (xmax + fov_radius) / fov_radius / 2
+    ymax_ = ymax / fov_radius
+    x_ymax_ = (x_ymax + fov_radius) / fov_radius / 2
 
-    return xmin_, xmax_, alpha_, yp_, a1_, a2_
+    return xmin_, xmax_, ymax_, x_ymax_
 
 
-def inverse_transform_reachsetparam(xmin_, xmax_, alpha_, yp_, a1_, a2_, alt, fov=FOV):
+def inverse_transform_reachsetparam(xmin_, xmax_, ymax_, x_ymax_, alt, fov=FOV):
     # inverse normalize
-    fov_radius = (alt * np.tan(fov / 2)) * 1.1  # 1.1 is a safety factor
-    length_max = fov_radius * 5
+    fov_radius = (alt * np.tan(fov / 2)) * 1.05  # 5% safety factor
     xmin = xmin_ * fov_radius * 2 - fov_radius
-    xmax = xmax_ * fov_radius * 2 + xmin
-    alpha = alpha_
-    yp = yp_ * fov_radius
-    a1 = a1_ * length_max + alpha * (xmax - xmin) / 2
-    a2 = a2_ * length_max + (1 - alpha) * (xmax - xmin) / 2
+    xmax = xmax_ * fov_radius * 2 - fov_radius
+    ymax = ymax_ * fov_radius
+    x_ymax = x_ymax_ * fov_radius * 2 - fov_radius
 
-    return xmin, xmax, alpha, yp, a1, a2
+    return xmin, xmax, ymax, x_ymax
